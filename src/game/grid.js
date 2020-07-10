132db88d459f1456fde839d18e543a3f0aaa84e7
Grady Shoemaker <grady404@gmail.com>
2020-07-10 16:22:09 -0700

    Tile/wall getters/setters now use the maps

--- src/game/grid.js
+++ src/game/grid.js
@@ -2,7 +2,7 @@ import {Vector} from "./vector.js";
 import {clamp, Orientation} from "./util.js";
 
 export class Grid {
-	constructor(width=0, height=0) {
+	constructor(width = 0, height = 0) {
 		this.size = new Vector(width, height)
 		this.tiles = []
 		this.verticalWalls = []
@@ -20,26 +20,38 @@ export class Grid {
 	}
 
 	setTile(x, y, type) {
-		let tile = new type();
 		if (x === typeof Vector) return this.setTile(x.x, x.y, y)
-		tile.x = clamp(x, 0, this.size.x)
-		tile.y = clamp(y, 0, this.size.y)
+		let tile = new type()
+		x = clamp(x, 0, this.size.x)
+		y = clamp(y, 0, this.size.y)
+		tile.x = x
+		tile.y = y
+		this.tiles[y][x] = tile
 	}
 
-	getTile(x, y=0) {
+	getTile(x, y) {
 		if (x === typeof Vector) return this.getTile(x.x, x.y)
-		return undefined
+		x = clamp(x, 0, this.size.x)
+		y = clamp(y, 0, this.size.y)
+		return this.tiles[y][x]
 	}
 
 	setWall(x, y, orientation, type) {
-		let wall = new type();
 		if (x === typeof Vector) return this.setTile(x.x, x.y, y, orientation)
-		wall.x = clamp(x, 0, this.size.x + (orientation === Orientation.VERTICAL ? -1 : 0))
-		wall.y = clamp(y, 0, this.size.y + (orientation === Orientation.HORIZONTAL ? -1 : 0))
+		let wall = new type()
+		x = clamp(x, 0, this.size.x + (orientation === Orientation.VERTICAL ? -1 : 0))
+		y = clamp(y, 0, this.size.y + (orientation === Orientation.HORIZONTAL ? -1 : 0))
+		wall.x = x
+		wall.y = y
+		if (orientation === Orientation.VERTICAL) this.verticalWalls[y][x] = wall
+		else if (orientation === Orientation.HORIZONTAL) this.horizontalWalls[y][x] = wall
 	}
 
 	getWall(x, y, orientation) {
 		if (x === typeof Vector) return this.getWall(x.x, x.y, y)
-		return undefined
+		x = clamp(x, 0, this.size.x + (orientation === Orientation.VERTICAL ? -1 : 0))
+		y = clamp(y, 0, this.size.y + (orientation === Orientation.HORIZONTAL ? -1 : 0))
+		if (orientation === Orientation.VERTICAL) return this.verticalWalls[y][x]
+		else if (orientation === Orientation.HORIZONTAL) return this.horizontalWalls[y][x]
 	}
 }
