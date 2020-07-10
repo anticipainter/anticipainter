3ebd83a4fbe6694fa1640dff2bc91eac55d5b435
Grady Shoemaker <grady404@gmail.com>
2020-07-10 16:13:09 -0700

    Generalized wall setter/getter functions
    The setters and getters for vertical and horizontal walls are now gone and replaced with general setters and getters for walls. Also, the previous code had a mistake in it (setVerticalWall called setHorizontalWall instead of setVerticalWall)

--- src/game/grid.js
+++ src/game/grid.js
@@ -1,5 +1,5 @@
 import {Vector} from "./vector.js";
-import {clamp} from "./util.js";
+import {clamp, Orientation} from "./util.js";
 
 export class Grid {
 	constructor(width=0, height=0) {
@@ -31,27 +31,15 @@ export class Grid {
 		return undefined
 	}
 
-	setHorizontalWall(x, y, type) {
+	setWall(x, y, orientation, type) {
 		let wall = new type();
-		if (x === typeof Vector) return this.setHorizontalWall(x.x, x.y, y)
-		wall.x = clamp(x, 0, this.size.x)
-		wall.y = clamp(y, 0, this.size.y - 1)
+		if (x === typeof Vector) return this.setTile(x.x, x.y, y, orientation)
+		wall.x = clamp(x, 0, this.size.x + (orientation === Orientation.VERTICAL ? -1 : 0))
+		wall.y = clamp(y, 0, this.size.y + (orientation === Orientation.HORIZONTAL ? -1 : 0))
 	}
 
-	getHorizontalWall(x, y) {
-		if (x === typeof Vector) return this.getHorizontalWall(x.x, x.y)
-		return undefined
-	}
-
-	setVerticalWall(x, y, type) {
-		let wall = new type();
-		if (x === typeof Vector) return this.setHorizontalWall(x.x, x.y, y)
-		wall.x = clamp(x, 0, this.size.x - 1)
-		wall.y = clamp(y, 0, this.size.y)
-	}
-
-	getVerticalWall(x, y) {
-		if (x === typeof Vector) return this.getVerticalWall(x.x, x.y)
+	getWall(x, y, orientation) {
+		if (x === typeof Vector) return this.getWall(x.x, x.y, y)
 		return undefined
 	}
 }
