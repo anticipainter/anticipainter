e62c870230fb206bc8dcaa1d4b3b15e377d8893b
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-10 15:33:08 -0500

    Updated convention in vector.js

--- src/game/vector.js
+++ src/game/vector.js
@@ -4,50 +4,38 @@ export class Vector {
 		this.y = y;
 	}
 
-	static Add(a, b) {
+	static add(a, b) {
 		return new Vector(a.x + b.x, a.y + b.y);
 	}
 
-	static Sub(a, b) {
+	static sub(a, b) {
 		return new Vector(a.x - b.x, a.y - b.y);
 	}
 
-	static Mul(a, b) {
+	static mul(a, b) {
 		return new Vector(a.x * b, a.y * b);
 	}
 
-	static Div(a, b) {
+	static div(a, b) {
 		return (b === 0) ? new Vector(a.x / b, a.y / b) : new Vector();
 	}
 
-	static Rotate(vector, degrees=90) {
+	static rotate(vector, degrees=90) {
 		let rotated = new Vector();
 		rotated.x = Math.round(vector.x * Math.cos(degrees * 0.0174533) - vector.y * Math.sin(degrees * 0.0174533));
 		rotated.y = Math.round(vector.x * Math.sin(degrees * 0.0174533) + vector.y * Math.cos(degrees * 0.0174533));
 		return rotated;
 	}
 
-	static FromList(list) {
+	static fromList(list) {
 		return new Vector(list[0], list[1]);
 	}
 
-	static ToList(vector) {
+	static toList(vector) {
 		return [vector.x, vector.y];
 	}
 
-	static ToString(vector) {
+	static toString(vector) {
 		return `${vector.x}-${vector.y}`;
-	}
-
-	static FromDirection(direction) {
-		if (direction === "up") {
-			return new Vector(0, -1);
-		} else if (direction === "down") {
-			return new Vector(0, 1);
-		} else if (direction === "left") {
-			return new Vector(-1, 0);
-		} else if (direction === "right") {
-			return new Vector(1, 0);
-		}
 	}
 }
