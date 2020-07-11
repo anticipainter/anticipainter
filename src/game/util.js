487d37562cddff40e33deaf4f175730f1cb3c315
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-10 22:30:33 -0500

    Added util functions to Direction and Orientation

--- src/game/util.js
+++ src/game/util.js
@@ -3,5 +3,49 @@ export function clamp(value, minimum, maximum) {
 }
 
 let i = 0
-export const Direction = { UP: i++, DOWN: i++, LEFT: i++, RIGHT: i++ }
-export const Orientation = { HORIZONTAL: i++, VERTICAL: i++ }
+
+export const Direction = {
+	LEFT: i++,
+	RIGHT: i++,
+	UP: i++,
+	DOWN: i++,
+	inverse(direction) {
+		if (direction === this.LEFT) return this.RIGHT
+		if (direction === this.RIGHT) return this.LEFT
+		if (direction === this.UP) return this.DOWN
+		if (direction === this.DOWN) return this.UP
+	},
+	leftOf(direction) {
+		if (direction === this.LEFT) return this.DOWN
+		if (direction === this.RIGHT) return this.UP
+		if (direction === this.UP) return this.LEFT
+		if (direction === this.DOWN) return this.RIGHT
+	},
+	rightOf(direction) {
+		if (direction === this.LEFT) return this.UP
+		if (direction === this.RIGHT) return this.DOWN
+		if (direction === this.UP) return this.RIGHT
+		if (direction === this.DOWN) return this.LEFT
+	},
+	all() {
+		return [this.LEFT, this.RIGHT, this.UP, this.DOWN]
+	},
+	random() {
+		return this.all()[Math.floor(Math.random() * 4)]
+	}
+}
+
+export const Orientation = {
+	HORIZONTAL: i++,
+	VERTICAL: i++,
+	flip(orientation) {
+		if (orientation === this.HORIZONTAL) return this.VERTICAL
+		if (orientation === this.VERTICAL) return this.HORIZONTAL
+	},
+	all() {
+		return [this.HORIZONTAL, this.VERTICAL]
+	},
+	random() {
+		return this.all()[Math.floor(Math.random() * 2)]
+	}
+}
