ed59ae087ddbbaf38fabd2e3692851007f1d922f
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-11 16:25:50 -0500

    Fixed code style

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = new PIXI.Application({
 	antialias: true,
 	backgroundColor: 0x263238
 })
+
 $("#game").append(app.view)
 app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
@@ -13,15 +14,17 @@ app.renderer.resize(window.innerWidth, window.innerHeight);
 let game = new Game(app)
 
 let keys = []
-$(document).keydown(function(event) {
+$(document).on("keydown", function(event) {
 	if (!keys.includes(event.which)) {
 		keys.push(event.which)
 		game.keyPress(event.which)
 	}
 })
-$(document).keyup(function(event) {
+
+$(document).on("keyup", function(event) {
 	keys = keys.filter(key => key !== event.which)
 })
-$(document).blur(function() {
+
+$(document).on("blur", function() {
 	keys.length = 0
 })
