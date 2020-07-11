c6d025eb2a4fceeecd88ddc321138d59f027dc42
Grady Shoemaker <grady404@gmail.com>
2020-07-11 15:44:23 -0700

    Rewrote movement code, added bonking
    Movement code now moves the sprite gradually, while moving the player's position in fixed increments behind the scenes.

--- src/game/player.js
+++ src/game/player.js
@@ -4,9 +4,11 @@ import {Direction, Orientation} from "./util.js"
 
 export class Player extends Entity {
 	game = undefined
-	moveStart = undefined
 	currentMove = undefined
 	nextMove = undefined
+	lastPosition = undefined
+	bonk = false
+	lerp = 0
 
 	constructor(game) {
 		super()
@@ -19,29 +21,41 @@ export class Player extends Entity {
 
 	checkWall(position, direction) {
 		if (direction === Direction.LEFT) return this.game.grid.getWall(position.x, position.y, Orientation.VERTICAL) !== undefined
-		else if (direction === Direction.RIGHT) return this.game.grid.getWall(position.x + 1, position.y, Orientation.VERTICAL) !== undefined
-		else if (direction === Direction.UP) return this.game.grid.getWall(position.x, position.y, Orientation.HORIZONTAL) !== undefined
-		else if (direction === Direction.DOWN) return this.game.grid.getWall(position.x, position.y + 1, Orientation.HORIZONTAL) !== undefined
+		if (direction === Direction.RIGHT) return this.game.grid.getWall(position.x + 1, position.y, Orientation.VERTICAL) !== undefined
+		if (direction === Direction.UP) return this.game.grid.getWall(position.x, position.y, Orientation.HORIZONTAL) !== undefined
+		if (direction === Direction.DOWN) return this.game.grid.getWall(position.x, position.y + 1, Orientation.HORIZONTAL) !== undefined
 	}
 
 	update() {
 		if (this.currentMove === undefined && this.nextMove !== undefined) {
 			this.currentMove = this.nextMove
-			this.moveStart = this.position
 			this.nextMove = undefined
+			this.bonk = this.checkWall(this.position, this.currentMove)
+			if (!this.bonk) {
+				this.lastPosition = this.position
+				this.position = Vector.add(this.position, Direction.toVector(this.currentMove))
+			}
+			this.lerp = 0
 		}
 		if (this.currentMove !== undefined) {
-			if (Vector.equals(this.position, Vector.add(this.moveStart, Direction.toVector(this.currentMove)))) {
-				this.currentMove = undefined
-			} else if (this.checkWall(this.moveStart, this.currentMove)) {
-				this.currentMove = undefined
+			this.lerp += 0.2
+			let position
+			if (this.bonk) {
+				let bonkPosition = Vector.add(this.position, Vector.mul(Direction.toVector(this.currentMove), 0.25))
+				if (this.lerp < 0.25) {
+					position = Vector.lerp(this.position, bonkPosition, this.lerp / 0.25)
+				} else if (this.lerp < 0.75) {
+					position = bonkPosition
+				} else {
+					position = Vector.lerp(bonkPosition, this.position, (this.lerp - 0.75) / 0.25)
+				}
 			} else {
-				this.position = Vector.add(this.position, Vector.mul(Direction.toVector(this.currentMove), 0.2))
-				this.position.x = Math.round(this.position.x * 10) * 0.1
-				this.position.y = Math.round(this.position.y * 10) * 0.1
+				position = Vector.lerp(this.lastPosition, this.position, this.lerp)
 			}
+			this.sprite.x = position.x * 64
+			this.sprite.y = position.y * 64
+			if (this.lerp >= 1) this.currentMove = undefined
 		}
-		super.update()
 	}
 
 	static getRegistryName() {
