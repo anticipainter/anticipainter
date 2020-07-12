07ae7352a3e361b3efced6ddc58f7d9a7d578ce2
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-12 08:32:41 -0500

    Added test rasterization code

---
+++ js/rasterize.js
@@ -0,0 +1,17 @@
+const nodePandoc = require('node-pandoc');
+
+const out = "../out/raster"
+
+function callback(err, result) {
+	if (err) console.error('Oh Nos: ',err)
+	console.log(result)
+	return result
+}
+
+function cover() {
+	let src = "../app/rec/layout/cover.svg"
+	let args = "-f svg -t pdf"
+	nodePandoc(src, args, callback)
+}
+
+cover()
