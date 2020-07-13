63da64de4d43dbf31d165b9edf85085405c0ce10
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-13 18:19:48 -0500

    Added cover logo to loading screen

--- app/src/index.js
+++ app/src/index.js
@@ -8,3 +8,13 @@ title.fadeTo("slow", 1, () => {
 		})
 	}, 250)
 })
+
+title.load("res/layout/cover.svg")
+
+let interval = setInterval(() => {
+	let text = $("text")
+	if (text.length !== 0) {
+		clearInterval(interval)
+		text.attr("y", -1000)
+	}
+}, 1)
