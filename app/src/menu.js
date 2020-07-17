4a677c0baa1b140a2880e94c5b1602b421737cb2
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-17 18:44:43 -0500

    Added eye blinking to the menu

--- app/src/menu.js
+++ app/src/menu.js
@@ -2,6 +2,8 @@ let body = $("body")
 
 body.fadeTo("slow", 1, () => { })
 
+$("#logo").load("res/layout/logo.svg")
+
 $("#play").on("click", e => {
 	body.fadeTo("fast", 0, () => {
 		window.location = "play.html"
@@ -23,3 +25,13 @@ $("#quit").on("click", e => {
 $(document).on("keydown", (e) => {
 	if (e.code === "Escape") window.close()
 })
+
+let frame = 150
+setInterval(() => {
+	let loop = (frame++) % 300
+	if (loop < 15) {
+		let percent = loop / 15
+		let opacity = percent < 0.5 ? 1 - 2 * percent : 2 * percent - 1
+		$("#eyes").attr("opacity", opacity)
+	}
+}, 1000 / 60)
