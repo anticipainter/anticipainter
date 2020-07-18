909efd2bb262322a0e76c45802b4c73a2549c211
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-18 01:08:27 -0500

    Revamped menu

--- app/src/menu.js
+++ app/src/menu.js
@@ -2,7 +2,7 @@ let body = $("body")
 
 body.fadeTo("slow", 1, () => { })
 
-$("#logo").load("res/layout/logo.svg")
+$("#logo").load("res/layout/cover.svg")
 
 $("#play").on("click", e => {
 	body.fadeTo("fast", 0, () => {
@@ -26,12 +26,54 @@ $(document).on("keydown", (e) => {
 	if (e.code === "Escape") window.close()
 })
 
-let frame = 150
-setInterval(() => {
-	let loop = (frame++) % 300
-	if (loop <= 15) {
-		let percent = loop / 15
-		let opacity = percent < 0.5 ? 1 - 2 * percent : 2 * percent - 1
-		$("#eyes").attr("opacity", opacity)
+function wink() {
+	let eye = $("#right-open")
+	eye.css("to", 0)
+	eye.stop()
+	eye.animate({to: 1}, {
+		duration: 500,
+		step: now => {
+			let opacity = now < 0.5 ? 1 - 4 * now : 4 * now - 3
+			eye.attr("opacity", Math.max(opacity, 0))
+		}
+	})
+}
+
+setTimeout(() => {
+	let text = $("text")
+	text.animate({test: 1.0}, {
+		duration: 1000,
+		step: now => {
+			text.attr("opacity", now)
+			text.attr("transform", `translate(${(1-now) * 200})`)
+		}
+	})
+}, 200)
+
+let buttons = $("#buttons")
+$(() => {
+	buttons.children().each((index, button) => {
+		$(button).css("opacity", 0)
+		setTimeout(() => {
+			let btn = $(button).find("button")
+			$(button).animate({opacity: 1}, {
+				duration: 500,
+				step: now => {
+					btn.css("top", (1-now) * 20)
+				}
+			})
+		}, 500 + 250 * index)
+	})
+})
+
+let interval = setInterval(() => {
+	let text = $("text")
+	if (text.length !== 0) {
+		clearInterval(interval)
+		text.attr("y", -1000)
+		text.attr("opacity", 0)
+		let player = $("#player")
+		player.addClass("mouse")
+		player.attr("onclick", "wink()")
 	}
-}, 1000 / 60)
+}, 1)
