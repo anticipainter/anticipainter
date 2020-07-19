4f7c52a55714ba230dcd7b25cb6e13e0576f1a19
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-18 23:10:43 -0500

    Added capability to save preferences

--- js/preload.js
+++ js/preload.js
@@ -1,4 +1,31 @@
+const fs = require("fs")
+const path = require("path")
 const shell = require("electron").shell
+const userDataPath = require("electron").remote.app.getPath("userData");
+
+Preferences = class {
+	constructor(opts) {
+		this.path = path.join(userDataPath, opts.configName + ".json")
+		this.data = parseDataFile(this.path, opts.defaults)
+	}
+
+	get(key) {
+		return this.data[key]
+	}
+
+	set(key, value) {
+		this.data[key] = value
+		fs.writeFileSync(this.path, JSON.stringify(this.data))
+	}
+}
+
+function parseDataFile(filePath, defaults) {
+	try {
+		return JSON.parse(fs.readFileSync(filePath))
+	} catch(error) {
+		return defaults
+	}
+}
 
 preload = {
 	openExternal: function(url) {
