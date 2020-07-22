cd354a2000ca5fc39804dc2480c88525a9dc47cc
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-21 20:30:29 -0700

    Added modal capabilities

---
+++ app/src/modal.js
@@ -0,0 +1,29 @@
+let view = $("#modals")
+let modals = {}
+
+view.children().each((index, item) => {
+	modals[$(item).attr("id")] = $(item)
+})
+
+function hideAll() {
+	for (let name in modals) modals[name].css("display", "none")
+}
+
+function hideBackground(complete) {
+	view.animate({opacity: 0}, 300, () => {
+		view.css("opacity", 1)
+		view.css("display", "none")
+		if (complete) complete()
+	})
+}
+
+function showModal(name) {
+	let modal = modals[name]
+	modal.load("src/" + name + ".html")
+	modal.css("display", "block")
+	view.css("display", "block")
+}
+
+$(window).on("click", e => {
+	if ($(e.target).attr("id") === "modals") hideBackground(hideAll)
+})
