cc7fa5d31e815fdc240e64d592580f8a2e164430
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-23 03:20:56 -0700

    Added isModalActive

--- app/src/modal.js
+++ app/src/modal.js
@@ -1,11 +1,13 @@
 let view = $("#modals")
 let modals = {}
+let activeModal = undefined
 
 view.children().each((index, item) => {
 	modals[$(item).attr("id")] = $(item)
 })
 
 function hideAll() {
+	activeModal = undefined
 	for (let name in modals) {
 		modals[name].html("")
 		modals[name].css("display", "none")
@@ -16,15 +18,21 @@ function hideBackground(complete) {
 	view.animate({opacity: 0}, 300, () => {
 		view.css("opacity", 1)
 		view.css("display", "none")
+		activeModal = undefined
 		if (complete) complete()
 	})
 }
 
 function showModal(name) {
+	activeModal = name
 	let modal = modals[name]
 	modal.load(name + ".html")
 	modal.css("display", "block")
 	view.css("display", "block")
+}
+
+function isModalActive(name) {
+	return name === activeModal
 }
 
 $(window).on("click", e => {
