5995912dfbbe09669d079dee846e6dc97bc115a1
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-23 04:10:57 -0700

    Added custom checkboxes

--- app/src/modal-settings.js
+++ app/src/modal-settings.js
@@ -1,16 +1,36 @@
 preferences = new Preferences({configName: "user-preferences", defaults: Preferences.defaults})
 
+$(".checkbox>span").load("res/drawable/checkbox.svg")
+
 $(() => {
-	$("#screen-shake").prop("checked", preferences.get("video.screen-shake"))
-	$("#chevrons").prop("checked", preferences.get("video.chevrons"))
+	setCheckbox("#screen-shake", preferences.get("video.screen-shake"))
+	setCheckbox("#chevrons", preferences.get("video.chevrons"))
 	$("#volume-master").val(preferences.get("audio.volume-master"))
 	$("#volume-music").val(preferences.get("audio.volume-music"))
 	$("#volume-sound").val(preferences.get("audio.volume-sound"))
-	$("#pitch-shift").prop("checked", preferences.get("audio.pitch-shift"))
+	setCheckbox("#pitch-shift", preferences.get("audio.pitch-shift"))
 })
 
+function setCheckbox(id, value) {
+	console.log(id)
+	console.log(value)
+	$(id).prop("checked", value)
+	$(id).parent().find("#cutout").attr("opacity", value ? 1 : 0)
+}
+
 $("input[type=checkbox]").on("input", (event) => {
-	preferences.set($(event.target).parent().parent().attr("id") + "." + $(event.target).attr("id"), $(event.target).prop("checked"))
+	let type = $(event.target).parent().parent().parent().attr("id")
+	let id = $(event.target).attr("id")
+	let checked = $(event.target).prop("checked")
+	let cutout = $(event.target).parent().find("#cutout")
+	cutout.css("now", 0)
+	cutout.animate({now: 1}, {
+		duration: 200,
+		step: now => {
+			cutout.attr("opacity", checked ? now : 1 - now)
+		}
+	})
+	preferences.set(type + '.' + id, checked)
 	preferences.write()
 })
 
