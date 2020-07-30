f31164d0d27d7832d0c2dbd42a377a678f8c76b8
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-29 18:12:43 -0700

    Recommented EventTile

--- app/src/dev/event/tile/event-tile.js
+++ app/src/dev/event/tile/event-tile.js
@@ -1,20 +1,21 @@
 import EventStage from "../stage/event-stage.js"
 
+/**
+ * Called when a change is made to a {@link Tile}
+ * @event EventTile
+ *
+ * @param {Stage} stage - reference to the {@link Stage} instance
+ * @param {Tile} tile - the {@link Tile} being changed
+ */
 export default class EventTile extends EventStage {
 	// region Properties
 	/**
 	 * Reference to the {@link Tile} instance
-	 * @property tile
 	 * @type {Tile}
 	 */
 	tile
 	// endregion
 
-	/**
-	 * @constructor
-	 * @param {Stage} stage
-	 * @param {Tile} tile
-	 */
 	constructor(stage, tile) {
 		super(stage)
 		this.tile = tile
