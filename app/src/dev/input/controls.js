9bbe4808e2d12517104bee8e822b9fc7885b2e4f
Cooper Anderson <cooper@cooperstuff.ninja>
2020-07-29 18:35:03 -0700

    Recommented Controls module

--- app/src/dev/input/controls.js
+++ app/src/dev/input/controls.js
@@ -1,8 +1,39 @@
 import GameModule from "../game-module.js";
 
+/**
+ * The controls module
+ * @class Controls
+ * @extends GameModule
+ *
+ * @param {Anticipainter} game - Reference to the game instance
+ */
 export default class Controls extends GameModule {
+	/**
+	 * Keys for moving left
+	 * @type {number[]}
+	 *
+	 * @memberOf Controls
+	 */
 	static MOVE_LEFT = [37, 65]
+	/**
+	 * Keys for moving right
+	 * @type {number[]}
+	 *
+	 * @memberOf Controls
+	 */
 	static MOVE_RIGHT = [39, 68]
+	/**
+	 * Keys for moving up
+	 * @type {number[]}
+	 *
+	 * @memberOf Controls
+	 */
 	static MOVE_UP = [38, 87]
+	/**
+	 * Keys for moving down
+	 * @type {number[]}
+	 *
+	 * @memberOf Controls
+	 */
 	static MOVE_DOWN = [40, 83]
 }
